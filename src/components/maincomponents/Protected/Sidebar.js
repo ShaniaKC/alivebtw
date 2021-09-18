70efdea853c795b5dd71e66692c16ad307ed6cf6
ShaniaKC <samuelmiraclekelechi@gmail.com>
2021-09-18 16:20:32 +0100

    2 files modified, 7 added, 1 deleted

--- src/components/maincomponents/Protected/Sidebar.js
+++ src/components/maincomponents/Protected/Sidebar.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import IconLink from '../../minorcomponents/IconLinkGroup';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import UpdateIcon from '@mui/icons-material/Update';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import FolderIcon from '@mui/icons-material/Folder';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 
-const A = () => {
-  return <div />;
+const Sidebar = () => {
+  return (
+    <div className="col-sm-2 vh-100 pt-5 border-primary bg-white border-top border-end border-5">
+      <IconLink to="/patient" text="Patient" icon={<PersonAddAlt1Icon />} />
+      <IconLink to="/booking" text="Booking" icon={<UpdateIcon />} />
+      <IconLink
+        to="/resources"
+        text="Resources"
+        icon={<BusinessCenterOutlinedIcon />}
+      />
+      <IconLink to="/medical" text="Medical" icon={<MedicalServicesIcon />} />
+      <IconLink to="/documents" text="Documents" icon={<FolderIcon />} />
+      <IconLink to="/visit" text="Visit" icon={<CreateOutlinedIcon />} />
+    </div>
+  );
 };
 
-export default A;
+export default Sidebar;
---
+++ src/components/minorcomponents/IconLinkGroup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const IconLink = (props) => {
+  return (
+    <Link className="row flex-d text-primary py-2 px-1" to={props.to}>
+      <span className="col-2">{props.icon}</span>{' '}
+      <span className="col-10">{props.text}</span>
+    </Link>
+  );
+};
+
+export default IconLink;
