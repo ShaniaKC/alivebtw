cb463bdde6178b11fb631f214f833796520aeb94
ShaniaKC <samuelmiraclekelechi@gmail.com>
2021-09-18 13:33:54 +0100

    1 file modified, 2 added, 6 deleted

---
+++ src/components/maincomponents/Protected/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+const Main = () => {
+  return (
+    <div className="container-fluid bg-white vh-100">
+      <div className="row">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/maincomponents/StationScreenTile.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const A = () => {
-  return <div />;
-};
-
-export default A;
