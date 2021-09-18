9a292e3589c156fde3070af47cc0df41b6e7e0aa
ShaniaKC <samuelmiraclekelechi@gmail.com>
2021-09-18 20:43:45 +0100

    1 file modified

--- src/components/maincomponents/Protected/Document.js
+++ src/components/maincomponents/Protected/Document.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Document = () => {
-  return <div>Document</div>;
+  return (
+    <div className="bg-white min-vh-100 mt-5 mx-md-3 border border-5 border-gray"></div>
+  );
 };
 
 export default Document;
